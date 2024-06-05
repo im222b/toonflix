f125df5f47d8c920d6f4de54017e8368d426b158
im222b <rudgnsdleh@naver.com>
2024-06-05 13:44:36 +0900

    day03-1

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,21 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
 
-  void onClickde() {
-    setState(() {
-      counter = counter + 1;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
+              Text(
                 'Click Count',
                 style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
-              IconButton(
-                iconSize: 40,
-                onPressed: onClickde,
-                icon: const Icon(
-                  Icons.add_box_rounded,
-                ),
               ),
             ],
           ),
